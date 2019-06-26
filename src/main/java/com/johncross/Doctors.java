38a6d905078491c737c8d7b37d52e87b3524a49c
John Cross <jwcross1@dmacc.edu>
2019-06-26 18:48:24 -0500

    added more functionality. need to fix removing appointment time from map after it is chosen.

--- src/main/java/com/johncross/Doctors.java
+++ src/main/java/com/johncross/Doctors.java
@@ -21,21 +21,44 @@ public class Doctors {
         doctorsAndTimes.put("Dr. Frankenstein", times);
         doctorsAndTimes.put("Dr. Seuss", times);
 
+
+        Boolean pickingDoctor = true;
+        while (pickingDoctor) {
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter doctor name: ");
         String name = scanner.nextLine();
-        if (doctorsAndTimes.containsKey(name)) {
-            System.out.println("\nAppointment times begin every hour starting at 8:00 and end at 12:00.");
-            System.out.println("Enter your desired appointment time: ");
-            String desiredAppointmentTime = scanner.next();
-            if (times.contains(desiredAppointmentTime)) {
-                System.out.println("sweet");
+            if (doctorsAndTimes.containsKey(name)) {
+                System.out.println("\nAppointment times begin every hour starting at 8:00 and end at 12:00.");
+                System.out.println("Enter your desired appointment time based on the available times listed below: ");
+                System.out.println(times.toString());
+                String desiredAppointmentTime = scanner.next();
+                if (times.contains(desiredAppointmentTime)) {
+                    System.out.println("Please confirm that you would like to schedule an appointment with " + name + " at " + desiredAppointmentTime + " by pressing y/n, followed by the enter key: ");
+                    String confirmAppointment = scanner.next();
+                    if (confirmAppointment.equalsIgnoreCase("y")) {
+                        doctorsAndTimes.remove(desiredAppointmentTime);
+//                        doctorsAndTimes.remove(name);
+//                        times.remove(desiredAppointmentTime);
+                        System.out.println("Awesome. We will schedule your appointment.");
+                        pickingDoctor = false;
+                    } else {
+                        System.out.println("Invalid input. Please try again!");
+                        pickingDoctor = true;
+                    }
+
+
+                } else {
+                    System.out.println("Appointment time is not available. Available times are listed below: ");
+                    System.out.println(times.toString());
                 }
-        } else {
-            System.out.println("Incorrect name entered.");
+            } else {
+                System.out.println("Incorrect name entered.");
+                pickingDoctor = true;
+
+            }
         }
     }
-
 
 
 }
